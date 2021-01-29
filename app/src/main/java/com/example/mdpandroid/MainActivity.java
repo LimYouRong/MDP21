b87040c6fe2c00c1c9d430230acb89593d4a61a6
YouRong <yourongl@hotmail.com>
2021-01-29 10:04:54 +0800

    Added Navigation panel into application. Able to save message. Todo: Bluetooth having issue with registering, need to

--- app/src/main/java/com/example/mdpandroid/MainActivity.java
+++ app/src/main/java/com/example/mdpandroid/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mdpandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.view.View;
 import android.view.Menu;
@@ -16,10 +21,16 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    //navbar
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    private String device;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -31,7 +42,42 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        setContentView(R.layout.activity_main);
 
+        //nav bar
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        dl.addDrawerListener(t);
+        t = new ActionBarDrawerToggle(this, dl,R.string.app_name, R.string.app_name);
+        t.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setItemIconTintList(null);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.home:
+                        dl.closeDrawers();
+                        return true;
+                    case R.id.bluetooth:
+                        Intent i = new Intent(MainActivity.this, BluetoothActivity.class);
+                        //send device name to next activity to allow app to know whether there is still bluetooth connection
+                        i.putExtra("device", device);
+                        startActivity(i);
+                        return true;
+                    case R.id.settings:
+                        Intent x = new Intent(MainActivity.this, SettingsActivity.class);
+                        //send device name to next activity to allow app to know whether there is still bluetooth connection
+                        x.putExtra("device", device);
+                        startActivity(x);
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
 
     }
 
